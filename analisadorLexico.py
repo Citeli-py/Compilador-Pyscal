01b6843912f564fc61097cf51a9b16e90f7bc940
Matheus Citeli <81636534+Citeli-py@users.noreply.github.com>
2023-10-26 11:31:21 -0300

    Add files via upload

--- analisadorLexico.py
+++ analisadorLexico.py
@@ -32,26 +32,27 @@ class AnalizadorLexico:
         'STRING',
         'CONST_VALOR',
         'COMENTARIO',
-        'ATRIBUICAO',
         'PONTOVIRGULA',
         'DOISPONTOS',
+        'ATRIBUICAO',
         'PONTO',
         'COLCHETE_ESQ',
         'COLCHETE_DIR',
         'PARENTESIS_ESQ',
         'PARENTESIS_DIR',
         'VIRGULA',
-    ]
-    tokens.extend(palavras_reservadas.values())
+        'IGUALIGUAL',
+    ] + list(palavras_reservadas.values())
 
-    t_NUMERO = r'[0-9]+(.[0-9]+)*'
+
+    t_NUMERO = r'[0-9]+(.[0-9]*)?'
     t_STRING = r'\".*\"'
     t_OP_MAT = r'[\+|\-|\*|\/]'
-    t_OP_LOGICO = r'[<|>|!|=]'
+    t_OP_LOGICO = r'[<|>|!|=]' 
 
-    t_ATRIBUICAO = r'\:='
     t_PONTOVIRGULA = r'\;'
     t_DOISPONTOS = r'\:'
+    t_ATRIBUICAO = r':='
     t_PONTO = r'\.'
     t_VIRGULA = r'\,'
     t_COLCHETE_ESQ = r'\['
@@ -59,8 +60,12 @@ class AnalizadorLexico:
     t_PARENTESIS_ESQ = r'\('
     t_PARENTESIS_DIR = r'\)'
 
+    def t_IGUALIGUAL(self, token: lex.LexToken) -> lex.LexToken:
+        r'=='
+        return token
+
     def t_ID(self, token: lex.LexToken) -> lex.LexToken: #Um identificador pode se confundir com uma palavra reservada
-        r'[a-zA-Z][a-zA-Z0-9]*'
+        r'[a-zA-Z][a-zA-Z0-9_]*'
         token.type = self.palavras_reservadas.get(token.value,'ID')    # Checa palavras reservadas
         return token
 
@@ -80,14 +85,19 @@ class AnalizadorLexico:
     t_ignore  = ' \t'
 
     def t_error(self, token: lex.LexToken) -> None:
-        print("\033[1;31;40m"+ 
-            f"Caracter ilegal '{token.value[0]}' na linha {token.lexer.lineno}" 
-            + "\033[0m")
+        self.erros_gerados.append((token.value, token.lineno))
         token.lexer.skip(1)
 
     def __init__(self,) -> None:
+        '''
+            token = (tipo, valor, linha)
+        '''
         self.lexer = lex.lex(module=self)
         self.tokens_gerados = list()
+        self.erros_gerados = list()
+
+    def addToken(self, token: lex.LexToken) -> None:
+        self.tokens_gerados.append((token.type, token.value, token.lineno))
 
     
     def tokenizar(self, programa: str) -> None:
@@ -98,9 +108,15 @@ class AnalizadorLexico:
             if not token:
                 break
 
-            self.tokens_gerados.append(token)
+            self.addToken(token)
 
     def printTokens(self,):
         for token in self.tokens_gerados:
-            print(f"{token.type} {(15-len(token.type))*' '} linha: {token.lineno} {(3-len(str(token.lineno)))*' '} {token.value}")
+            print(f"{token[0]} {(15-len(token[0]))*' '} linha: {token[2]} {(3-len(str(token[2])))*' '} {token[1]}")
+    
+    def printErros(self, ):
+        for token in self.erros_gerados:
+            print("\033[1;31;40m"+ 
+                f"Caracter ilegal '{token[0][0]}' na linha {token[1]}" 
+                + "\033[0m")
 
