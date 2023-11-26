6d5e28303dff00c36a4b821ee4282cbebc82d043
Matheus Citeli <81636534+Citeli-py@users.noreply.github.com>
2023-11-26 20:09:26 -0300

    atualização semantico

--- analisadorSemantico.py
+++ analisadorSemantico.py
@@ -23,11 +23,14 @@ class TabelaSimbolos:
     
     def add(self, simbolo) -> None: #Melhorar isso ai, criar um simbolo em cada função
         # (Nome, Classificação, Tipo, Escopo, Quantidade, Ordem)
-        if simbolo[0] not in self.simbolos.keys():
-            self.simbolos[(simbolo[0], simbolo[3])] = simbolo
-            printg(simbolo)
-        else:
-            print(f"Erro: Já existe o ID {simbolo[0]} em {simbolo[3]}")
+        if (simbolo[0], simbolo[3]) not in self.simbolos.keys():
+            if (simbolo[0], "GLOBAL") not in self.simbolos.keys():
+                self.simbolos[(simbolo[0], simbolo[3])] = simbolo
+                printg(simbolo)
+                return True
+    
+        printv(f"Erro: Já existe o ID {simbolo[0]} em {simbolo[3]}")
+        return False
 
     def lookup(self, nome, escopo):
        res = self.simbolos.get((nome, escopo)) 
@@ -47,36 +50,75 @@ class AnalisadorSemantico:
             for filho in no:
                 if isinstance(filho, tuple):
                     self.escopoAtual(filho)
-                    self.regras(filho)
-                    self.semantico(filho)
+                    if self.regras(filho):
+                        self.semantico(filho)
     
     def escopoAtual(self, no): # Função pra trocar de escopo
         if no[0] == "DEF_ROT":
             self.escopo = no[1][0][1][1][1]
     
     def regras(self, no):
+
         if no[0] == 'CONSTANTE':
             self.constante(no[1])
+            return False
         
         elif no[0] == "TIPO":
             self.tipo(no[1])
+            return False
         
         elif no[0] == 'VARIAVEL':
             self.variavel(no[1])
+            return False
+        
+        elif no[0] == "NOME_ROTINA":
+            self.nome_rotina(no[1])
+            return False
+        
+        # Preciso tratar nomes
+        # Existem nomes em: COMANDOS(ATRIB e READ), NOME e PARAMETRO
+        # Tratar cada tipo de nome (Chamada de função, campo de registro e acesso em array)
+        elif no[0] == "COMANDO":
+            #print(no)
+            if no[1][0][0] == "READ":
+                no_nome = no[1][2]
+                identificador = no[1][1][1]
+                printg(no_nome, self.nome(identificador, no_nome))
+            
+            elif no[1][0][0] == "ID" and no[1][2] != None: # Atribuição
+
+                # ('COMANDO', (('ID', 'i', 40), None, ('ATRIB', (('ATRIBUICAO', ':=', 40), ('EXP_MAT', ('PARAMETRO', ('NUMERO', '1', 40)))))))
+                print(no[1])
+                # Termo a esquerda
+                if no[1][1] == None:
+                    tipo_esq = self.Tabela.lookup(no[1][0][1], self.escopo)[2] # Checar se existe na tabela
+
+                else: # Caso tenha nome
+                    pass
+
+                no_dir = no[1][2][1][1][1]
+                printv(no_dir)
+                id_dir = no_dir
+
+
+            return False
 
         elif no[0] == 'ID': # Verifica se o ID existe na tabela
-            # Falta inteligencia aqui
+            # Lidar com variaveis record
             if self.Tabela.lookup(no[1], self.escopo) == None:
-                # Problema com IDs em RECORDS
                 printv(f"ERRO: {no} não foi declarado!")
+            return False
 
         else:
-            pass
-
-    
+            return True
+ 
     def constante(self, no):
         # nome, CONSTANTE, CONSTANTE, escopo, None, None 
-        simbolo = (no[0][1], 'CONSTANTE', 'CONSTANTE', self.escopo, None, None) 
+        if no[2][1][0] == "EXP_MAT":
+            simbolo = (no[0][1], 'CONSTANTE', 'NUMERO', self.escopo, None, None) 
+        else:
+            simbolo = (no[0][1], 'CONSTANTE', 'STRING', self.escopo, None, None) 
+
         self.Tabela.add(simbolo)
     
     def tipo(self, no):
@@ -86,36 +128,204 @@ class AnalisadorSemantico:
         self.Tabela.add(simbolo)
 
     def variavel(self, no):
-        # nome, VARIAVEL, tipo, escopo, qtd, ordem 
-        printv(no)
+        # nome, VARIAVEL, tipo, escopo, qtd, None 
+        var_tipo = self.tipo_dado(no[3])
         if no[1] == None: # Variavel sozinha
-            var_tipo = self.tipo_dado(no[3])
             simbolo = (no[0][1], "VARIAVEL", var_tipo[0], self.escopo, var_tipo[1], None)
             self.Tabela.add(simbolo)
         
-        else:
-            # Percorrer a lista id e ir adicionando na tabela
-            pass
-        
+        else: # Funciona fds
+            self.Tabela.add((no[0][1], "VARIAVEL", var_tipo[0], self.escopo, var_tipo[1], None))
 
+            lista_id = no[1][1]
+            self.Tabela.add((lista_id[1][1], "VARIAVEL", var_tipo[0], self.escopo, var_tipo[1], None))
+            while lista_id[2] != None:
+                lista_id = lista_id[2][1]
+                self.Tabela.add((lista_id[1][1], "VARIAVEL", var_tipo[0], self.escopo, var_tipo[1], None))
+
+    def param_rot(self, no):
+        # nome, PARAMETRO, tipo, escopo, qtd, ordem 
+        ordem = 1
+        campos = no[1][1]
+        param_tipo = self.tipo_dado(campos[2])
+        simbolos_param = [(campos[0][1], "PARAMETRO", param_tipo[0], self.escopo, param_tipo[1], ordem)]
+        self.Tabela.add(simbolos_param[0])
+        
+        while campos[3] != None:
+            ordem += 1
+            campos = campos[3][1][1][1]
+            param_tipo = self.tipo_dado(campos[2])
+            simbolos_param.append((campos[0][1], "PARAMETRO", param_tipo[0], self.escopo, param_tipo[1], ordem))
+            self.Tabela.add(simbolos_param[ordem-1])
+        
+        return simbolos_param
+
+    def nome_rotina(self, no):
+        if no[0][0] == "PROCEDURE":
+            self.Tabela.add((no[1][1], "PROCEDURE", None, "GLOBAL", None, None))
+        
+        elif no[0][0] == "FUNCTION":
+            # nome, TIPO, tipo, escopo, qtd, parametros 
+            func_tipo = self.tipo_dado(no[4])
+            parametros = self.param_rot(no[2][1])
+            self.Tabela.add((no[1][1], "FUNCTION", func_tipo[0], "GLOBAL", func_tipo[1], parametros))
+    
+    def nome_ponto(self, ID, no_nome):
+        simbolo = self.Tabela.lookup(ID, self.escopo)
+
+        if simbolo == None: # Verificar se o ID existe 
+            printv(f"ERRO: {ID} não foi declarado!")
+            return None
+        
+        # Verificar se o tipo do ID corresponde a um record existente
+        tipo = simbolo[2]
+        record = self.Tabela.lookup(tipo, self.escopo)
+
+        if record == None or record[2] != "RECORD":
+            printv(f"ERRO: {ID} é {simbolo[2]} não um RECORD!")
+            return None
+        
+        campo_nome = no_nome[1][1][1]
+        campos = record[4]
+
+        existe_campo = False
+        tipo_campo = None
+        for campo in campos:
+            if campo_nome == campo[0]:
+                existe_campo = True
+                tipo_campo = campo[2]
+                break
+        
+        if not existe_campo:
+            printv(f"ERRO: não existe {campo_nome} em {tipo}!")
+            return None
+        
+        return tipo_campo
+    
+    def nome_colchete(self, ID, no_nome):
+        simbolo_id = self.Tabela.lookup(ID, self.escopo)
+
+        if simbolo_id == None:
+            printv(f"ERRO: {ID} Não foi declarado")
+            return None
+
+        tipo = simbolo_id[2]
+
+        # (Nome, Classificação, Tipo, Escopo, Quantidade, Ordem)
+        simbolo_type = self.Tabela.lookup(tipo, self.escopo)
+        if simbolo_type == None: # ID é um integer ou real
+            simbolo = simbolo_id
+        else:
+            simbolo = simbolo_type
+
+        if simbolo[4] != None: # ID é um array
+            tamanho_array = int(simbolo[4])
+            id_index = int(no_nome[1][1][1][1]) # Tratar para o caso de ID
+            if id_index > tamanho_array or id_index < 1:
+                printv(f"ERRO: index não corresponde ao tamanho do array {simbolo_id[0]}")
+                return None
+        
+        else:
+            printv(f"ERRO: {simbolo_id[0]} não é um array")
+            return None
+
+        return simbolo[2]
+    
+    def conta_param(self, no, valor):
+        contador = 0
+        if no[0] == valor:
+            return 1
+        
+        else:
+            for filho in no:
+                if isinstance(filho, tuple):
+                    contador += self.conta_param(filho, valor)
+
+        return contador
+
+    def nome_parentesis(self, ID, no_nome):
+        # Checar se é função
+        # Ver se todos os parametros foram passados 
+        # Ver se os parametros correspondem aos tipos, caso seja um ID
+        simbolo_id = self.Tabela.lookup(ID, self.escopo)
+
+        if simbolo_id == None:
+            printv(f"ERRO: {ID} Não foi declarado")
+            return None
+        
+        if simbolo_id[1] != "FUNCTION":
+            printv(f"ERRO: {ID} Não é uma função")
+            return None
+        
+        tipo = simbolo_id[2]
+        
+        parametros_id = simbolo_id[5]
+        lista_params = no_nome[1][1]
+
+        num_parametros = self.conta_param(lista_params, "PARAMETRO")
+
+        if num_parametros != len(parametros_id):
+            printv(f"ERRO: função {ID} recebeu {num_parametros} parametros mas suporta apenas {len(parametros_id)}")
+            return None
+
+        parametros_declarados = []
+
+        for i in range(num_parametros-1):
+            param = lista_params[1][0][1]
+            parametros_declarados.append(param)
+            lista_params = lista_params[1][2]
+
+        param = lista_params[1][1]
+        parametros_declarados.append(param)
+
+        for i in range(len(parametros_declarados)):
+            if parametros_declarados[i][0] == "NUMERO":
+                if parametros_id[i][2] != "REAL" and parametros_id[i][2] != "INTEGER":
+                    printv(f"ERRO: O parametro {i+1} de {simbolo_id[0]} esperado é um {parametros_id[i][2]} ")
+            
+            else:
+                simbolo_param = self.Tabela.lookup(parametros_declarados[i][0][1], self.escopo)
+                if simbolo_param == None:
+                    printv(f"ERRO: O ID {parametros_declarados[i][0][1]} não foi declarado!")
+                
+                else:
+                    if simbolo_param[2] == "NUMERO":
+                        if parametros_id[i][2] != "REAL" and parametros_id[i][2] != "INTEGER":
+                            printv(f"ERRO: O parametro {i+1} de {simbolo_id[0]} esperado é um {parametros_id[i][2]} mas foi recebido {simbolo_param[i][2]}")
+                    
+                    elif simbolo_param[2] != parametros_id[i][2]:
+                        printv(f"ERRO: O parametro {i+1} de {simbolo_id[0]} esperado é um {parametros_id[i][2]} mas foi recebido {simbolo_param[2]}")
+
+
+        return tipo
+
+    def nome(self, ID, no_nome): # Retorna o tipo do nome 
+        if no_nome[1][0][0] == "PONTO": #Pertence a um record
+            return self.nome_ponto(ID, no_nome)
+        
+        elif no_nome[1][0][0] == "COLCHETE_ESQ": # Array
+            return self.nome_colchete(ID, no_nome)
+        
+        elif no_nome[1][0][0] == "PARENTESIS_ESQ":
+            return self.nome_parentesis(ID, no_nome)
+            
     def record(self, no):
         campos = no[1][1][1][1]
 
         simbolos_campos = []
-        tipo_record = self.tipo_dado(campos)
+        tipo_record = self.tipo_dado(campos[2])
         simbolos_campos.append((campos[0][1], 'CAMPO', tipo_record[0], self.escopo, tipo_record[1], None))
 
         prox_campo = campos
         while prox_campo[3] != None:
             prox_campo = prox_campo[3][1][1][1]
-            tipo_record = self.tipo_dado(prox_campo)
+            tipo_record = self.tipo_dado(prox_campo[2])
             simbolos_campos.append((prox_campo[0][1], 'CAMPO', tipo_record[0], self.escopo, tipo_record[1], None))
 
         return 'RECORD', simbolos_campos
 
     def array(self, no, ):
         return no[1][1][5][1][0], no[1][1][2][1] 
-
 
     def tipo_dado(self, no_tipo_dado): # Retorna o tipo e a qtd de um dado
         if no_tipo_dado[1][0] == 'ARRAY':
